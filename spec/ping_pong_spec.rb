c3d30645cecdfc208020a5cc05fef988292264d4
a.ficklin11@gmail.com and Bryce Frazier <bryce@>
2018-02-05 11:14:21 -0800

    add specs and logic

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,3 +1,20 @@
-Behavior: Counts from 1 to a given number
-Input: 2
-Output: an array of [1,2]
+require('rspec')
+require('ping_pong')
+
+describe('#ping_pong?') do
+  it("counts from 1 to a given number") do
+    expect(ping_pong?(2)).to(eq([1,2]))
+  end
+end
+
+describe('#ping_pong?') do
+  it("turns numbers divisible by 3 into 'ping'") do
+    expect(ping_pong?(3)).to(eq([1,2,"ping"]))
+  end
+end
+
+describe('#ping_pong?') do
+  it("turns numbers divisible by 5 into 'pong'") do
+    expect(ping_pong?(5)).to(eq([1,2,"ping",4,"pong"]))
+  end
+end
